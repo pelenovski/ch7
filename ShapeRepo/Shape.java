c6e92903785aa7979ff5c79e2dc90420129a11a4
pelenovski <31411489+pelenovski@users.noreply.github.com>
2018-03-20 18:02:50 -0500

    Shape Lab

--- ShapeRepo/Shape.java
+++ ShapeRepo/Shape.java
@@ -9,32 +9,39 @@ public abstract class Shape
 {
     private double xPos, yPos;
 
-    public Shape (double x, double  y)
+    public Shape ()
     {
-        
+        xPos = 0;
+        yPos = 0;
     }
-
+    
+    public Shape (double x, double y)
+    {
+        xPos = x;
+        yPos = y;
+    }
+    
+    
     // you must write these methods here
     public double getXpos()
     {
-        
+        return xPos;
     }
     public double getYpos()
     {
-        
+        return yPos;
     }
     //public void move(double xLoc, double yLoc);
     // increase the size by the factor
     //public void stretchby (double factor);
 
     public abstract double getArea();
-
-    public abstract void draw (Pen p);
-
+    
+    public abstract void draw(Pen p);
     //toString should display xpos, ypos
     public String toString()
     {
-        return //something something
+        return ("The circle coordinates are"+getXpos()+","+getYpos());
     }
 
 }//end of Shape
